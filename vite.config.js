922942dbb10e4a575cb62ba427c02dbdf63bd6e6
Maksim <mickeyrossgame@gmail.com>
2025-02-26 11:31:06 +0200

    vite config edditing

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Гарантирует, что пути работают на продакшене
+  build: {
+    outDir: 'dist', // Vercel ищет index.html в dist
+  },
+  server: {
+    open: true
+  }
 })
